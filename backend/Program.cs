25ef6d4fcff409a3e2b72a915baa9406f33f3e2f
GeThem <petr.a.cow.and.ray@gmail.com>
2025-05-26 22:46:30 +0300

    username requirements, admin management

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,7 @@ using AICourseTester.Data;
 using AICourseTester.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +38,26 @@ builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<MainDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    //// Lockout settings.
+    //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    //options.Lockout.MaxFailedAccessAttempts = 5;
+    //options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._+!@#$%^&*";
+});
+
 var app = builder.Build();
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -69,15 +90,35 @@ using (var scope = app.Services.CreateScope())
         }
     }
 
+
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
     var userStore = scope.ServiceProvider.GetRequiredService<IUserStore<ApplicationUser>>();
-    string userName = "admin";
-
+    string? userName = builder.Configuration["Admin:UserName"];
+    if (userName == null)
+    {
+        userName = "admin";
+    }
+    
     if (await userManager.FindByNameAsync(userName) == null)
     {
+        var password = ((string?[])[builder.Configuration["Admin:Password"], builder.Configuration["Admin:Pw"]]).FirstOrDefault(pw => pw != null);
+        if (password == null)
+        {
+            throw new Exception("Provide password for admin - secrets.json / appsettings.json:\n\t\"Admin\": { {\"Password\" | \"Pw\"}: \"<password>\"[, \"UserName\": \"<username>\"] }");
+        }
+
+        var ctx = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+        var role = await ctx.Roles.Where(r => r.Name == "Administrator").FirstAsync();
+        var prevAdmin = await ctx.UserRoles.FirstOrDefaultAsync(u => u.RoleId == role.Id);
+        if (prevAdmin != null)
+        {
+            await ctx.Users.Where(u => u.Id == prevAdmin.UserId).ExecuteDeleteAsync();
+            await ctx.SaveChangesAsync();
+        }
+        
         var user = new ApplicationUser();
         await userStore.SetUserNameAsync(user, userName, CancellationToken.None);
-        await userManager.CreateAsync(user, builder.Configuration["Admin:Pw"]);
+        await userManager.CreateAsync(user, password);
         await userManager.AddToRoleAsync(user, "Administrator");
     }
 }
