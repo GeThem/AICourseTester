aa6e33bf5fa7426bd41dcf4c75372bc51a208e1c
GeThem <petr.a.cow.and.ray@gmail.com>
2025-05-30 15:54:00 +0300

    add DTO

---
+++ backend/DTO/RegReq.cs
@@ -0,0 +1,16 @@
+﻿namespace AICourseTester.DTO
+{
+    //
+    // Summary:
+    //     The request type for the "/register" endpoint added by Microsoft.AspNetCore.Routing.IdentityApiEndpointRouteBuilderExtensions.MapIdentityApi``1(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder).
+    public sealed class RegReq
+    {
+        public RegReq() { }
+        public required string UserName { get; init; }
+        public required string Password { get; init; }
+        public string? Name { get; init; }
+        public string? SecondName { get; init; }
+        public string? Patronymic { get; init; }
+        public int? GroupId { get; init; }
+    }
+}
